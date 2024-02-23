889daabfeb93743aebebdd593259025dd319ab98
User <alicsebaiust01@gmail.com>
2024-02-24 02:34:22 +0600

    sign in page ui on progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 import 'package:form_page/form_page.dart';
 
@@ -18,6 +19,161 @@ class _MyAppState extends State<MyApp> {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LogInScreen(),
+    );
+  }
+}
+*/
+
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SalePro Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'SalePro',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              const SizedBox(height: 50),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'UserName',
+                  border: UnderlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: UnderlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.deepPurpleAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),),
+                  ),
+                  onPressed: () {
+                    // Handle login
+                  },
+                  child: const Text('Login',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 0.5,
+                  ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.cyan,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),),
+                    ),
+                    onPressed: () {
+                      // Handle different login types
+                    },
+                    child: const Text('Login as Admin',
+                      style: TextStyle(
+                       // fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.5,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.cyan,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),
+                    ),
+                    onPressed: () {
+                      // Handle different login types
+                    },
+                    child: const Text('Login as Staff',
+                      style: TextStyle(
+                        //fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.5,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),
+                    ),
+                    onPressed: () {
+                      // Handle different login types
+                    },
+                    child: const Text('Login as Customer',
+                      style: TextStyle(
+                        //fontSize: 16,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.5,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 32,),
+              TextButton(
+                onPressed: () {
+                  // Handle forgot password
+                },
+                child: const Text('Forgot Password?'),
+              ),
+
+              const Text('Do not have an account'),
+              TextButton(
+                onPressed: () {
+                  // Handle account registration
+                },
+                child: Text('Register'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
